2068dda39c0cd89eb461a1092514de8200de25ac
Martin Egan <martingegan@gmail.com>
2018-06-08 12:54:26 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+// styles
+import './App.scss'
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        What a load a shit
+      </div>
+    )
+  }
+}
+
+export default App
